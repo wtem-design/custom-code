24a090172514dd561d72defcc4f0ba6b356e601e
wtem-design <97365208+wtem-design@users.noreply.github.com>
2022-01-08 22:48:42 +0100

    Create scroll.js

---
+++ scroll.js
@@ -0,0 +1,100 @@
+<script>
+// z index change for the 'our services' section interactions
+
+document.addEventListener('DOMContentLoaded', () => {
+ document.querySelectorAll('#ci-trigger').forEach(trigger => {
+  trigger.addEventListener('mouseover', function(){ 
+    document.querySelectorAll('#ci').forEach(target => target.classList.add('z-index-high')); 
+  });
+ });     
+ document.querySelectorAll('#ci-trigger').forEach(trigger => {
+  trigger.addEventListener('mouseout', function(){ 
+    document.querySelectorAll('#ci').forEach(target => target.classList.remove('z-index-high')); 
+  });
+ }); 
+});
+document.addEventListener('DOMContentLoaded', () => {
+ document.querySelectorAll('#df-trigger').forEach(trigger => {
+  trigger.addEventListener('mouseover', function(){ 
+    document.querySelectorAll('#df').forEach(target => target.classList.add('z-index-high')); 
+  });
+ });     
+ document.querySelectorAll('#df-trigger').forEach(trigger => {
+  trigger.addEventListener('mouseout', function(){ 
+    document.querySelectorAll('#df').forEach(target => target.classList.remove('z-index-high')); 
+  });
+ }); 
+});
+document.addEventListener('DOMContentLoaded', () => {
+ document.querySelectorAll('#de-trigger').forEach(trigger => {
+  trigger.addEventListener('mouseover', function(){ 
+    document.querySelectorAll('#de').forEach(target => target.classList.add('z-index-high')); 
+  });
+ });     
+ document.querySelectorAll('#de-trigger').forEach(trigger => {
+  trigger.addEventListener('mouseout', function(){ 
+    document.querySelectorAll('#de').forEach(target => target.classList.remove('z-index-high')); 
+  });
+ }); 
+});
+document.addEventListener('DOMContentLoaded', () => {
+ document.querySelectorAll('#en-trigger').forEach(trigger => {
+  trigger.addEventListener('mouseover', function(){ 
+    document.querySelectorAll('#en').forEach(target => target.classList.add('z-index-high')); 
+  });
+ });     
+ document.querySelectorAll('#en-trigger').forEach(trigger => {
+  trigger.addEventListener('mouseout', function(){ 
+    document.querySelectorAll('#en').forEach(target => target.classList.remove('z-index-high')); 
+  });
+ }); 
+});
+document.addEventListener('DOMContentLoaded', () => {
+ document.querySelectorAll('#es-trigger').forEach(trigger => {
+  trigger.addEventListener('mouseover', function(){ 
+    document.querySelectorAll('#es').forEach(target => target.classList.add('z-index-high')); 
+  });
+ });     
+ document.querySelectorAll('#es-trigger').forEach(trigger => {
+  trigger.addEventListener('mouseout', function(){ 
+    document.querySelectorAll('#es').forEach(target => target.classList.remove('z-index-high')); 
+  });
+ }); 
+});
+document.addEventListener('DOMContentLoaded', () => {
+ document.querySelectorAll('#in-trigger').forEach(trigger => {
+  trigger.addEventListener('mouseover', function(){ 
+    document.querySelectorAll('#in').forEach(target => target.classList.add('z-index-high')); 
+  });
+ });     
+ document.querySelectorAll('#in-trigger').forEach(trigger => {
+  trigger.addEventListener('mouseout', function(){ 
+    document.querySelectorAll('#in').forEach(target => target.classList.remove('z-index-high')); 
+  });
+ }); 
+});
+	document.addEventListener('DOMContentLoaded', () => {
+ document.querySelectorAll('#la-trigger').forEach(trigger => {
+  trigger.addEventListener('mouseover', function(){ 
+    document.querySelectorAll('#ci').forEach(target => target.classList.add('z-index-high')); 
+  });
+ });     
+ document.querySelectorAll('#la-trigger').forEach(trigger => {
+  trigger.addEventListener('mouseout', function(){ 
+    document.querySelectorAll('#ci').forEach(target => target.classList.remove('z-index-high')); 
+  });
+ }); 
+});
+document.addEventListener('DOMContentLoaded', () => {
+ document.querySelectorAll('#rs-trigger').forEach(trigger => {
+  trigger.addEventListener('mouseover', function(){ 
+    document.querySelectorAll('#df').forEach(target => target.classList.add('z-index-high')); 
+  });
+ });     
+ document.querySelectorAll('#rs-trigger').forEach(trigger => {
+  trigger.addEventListener('mouseout', function(){ 
+    document.querySelectorAll('#df').forEach(target => target.classList.remove('z-index-high')); 
+  });
+ }); 
+});
+</script>
